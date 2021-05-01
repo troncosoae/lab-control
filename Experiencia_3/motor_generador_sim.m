8309594ff4483364c19e6e88ac7bb708b95574bd
kadawn0 <kadawn0@users.noreply.github.com>
2021-05-01 18:50:14 -0400

    just go

--- Experiencia_3/motor_generador_sim.m
+++ Experiencia_3/motor_generador_sim.m
@@ -15,8 +15,8 @@ J = 2.1 * 10^(-6);
 v_A0 = 2;
 omega_0 = 1500;
 
-R_f = R_as;
-C_f = L_as;
+R_f = 10000;
+C_f = 1000*10^(-9);
 
 % variable s es simbólica para que matlab haga álgebra por uno
 syms s
@@ -24,17 +24,23 @@ syms s
 num = [143134928095778949120000, 36192688961361248563200000000];
 den = [34709393649091889, 8873737467196072166715, 24583972753858281030182000, 42460474904292344280000000];
 
-K_p = 100000;
-K_i = 100000;
-K_d = 100000;
+K_p = 0.22;
+K_i = 0.26;
+K_d = 0.18;
+N = 186;
+pid = simplify(K_p + K_i*1/s + K_d*N/(1 + N*(1/s)));
+[N, D] = numden(pid);
 
-PID = tf([K_d, K_p, K_i], [1, 0]);
+PID = tf([ 2418 2059 1685], [ 9300 50]);
+% PID = tf([1 1], [5 0]);
+%PID = tf([K_d, K_p, K_i], [1, 0]);
 convertidor = tf([K_h], [T_h, 1]);
 sensor = tf([K_s], [R_f*C_f, 1]);
 
 % descomentar TF_motor_generador para ver la simplificación de la función
 % de transferencia original
 % TF_motor_generador = simplify((K_m/(R_am + L_am*s))/(B + J*s + (K_m^2/(R_am + L_am*s)) + (K_g^2/(R_ag + R_l + L_ag*s))));
+
 TF_mg = tf(num, den);
 TF_scontrol = TF_mg*convertidor/(1 + TF_mg*convertidor*sensor);
 
@@ -44,11 +50,11 @@ sistema = PID*TF_mg*convertidor/(1 + PID*TF_mg*convertidor*sensor);
 
 % rltool(TF_mg)
 % rltool(sistema)
-pidTuner(TF_scontrol)
+% pidTuner(TF_scontrol)
 
-rlocus(TF_mg, 'b', sistema, 'k')
+rlocus(TF_mg, 'b', sistema, 'k', TF_scontrol, 'r')
 hold on
-legend('Motor/Generador','Motor/Generador con controlador PID')
+legend('Motor/Generador','Motor/Generador con controlador PID', 'Motor Actuador sensor sin controlador')
 hold off
 
 
